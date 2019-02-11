158bae539d42ecd1f049c3ab8054a2675d5a237e
X-olani <xolani.sk.yona@gmail.com>
2019-02-11 22:20:27 +0200

    frist changes

---
+++ precache-manifest.fc605f0d3470f069fd338b5bae74bc16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "96e32b2b254ee7f30bbeddf9aa4c05ac",
+    "url": "/static/media/Hangman10.96e32b2b.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "48d73c9744d5875d3b09",
+    "url": "/static/js/main.48d73c97.chunk.js"
+  },
+  {
+    "revision": "7e4aa83bb1e123c540a7",
+    "url": "/static/js/1.7e4aa83b.chunk.js"
+  },
+  {
+    "revision": "48d73c9744d5875d3b09",
+    "url": "/static/css/main.f5112247.chunk.css"
+  },
+  {
+    "revision": "2f0cd00d4208eba81f1da73beaf1dd96",
+    "url": "/index.html"
+  }
+];
